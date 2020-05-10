fa3a667b4feb9423c40fd2b146ba1338b993d713
linnefromice <linnefromice@gmail.com>
2020-05-10 17:40:22 +0900

    ver gatsby qiita - add markdown reader & md file post

--- version_gatsby_qiita/src/pages/index.js
+++ version_gatsby_qiita/src/pages/index.js
@@ -8,12 +8,24 @@ import SEO from "../components/seo"
 import { useStaticQuery, graphql } from "gatsby"
 
 const IndexPage = () => {
-  const { site } = useStaticQuery(
+  const { site, allMarkdownRemark } = useStaticQuery(
     graphql`
-      {
+      query {
         site {
           siteMetadata {
             description
+          }
+        }
+        allMarkdownRemark {
+          nodes {
+            id
+            frontmatter {
+              title
+              path
+              date
+              tags
+            }
+            html
           }
         }
       }
@@ -28,6 +40,14 @@ const IndexPage = () => {
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
+      {allMarkdownRemark.nodes.map((node) => (
+        <div key={node.id}>
+          <h3>{node.frontmatter.title}</h3>
+          <p>{node.frontmatter.date}</p>
+          <p>{node.frontmatter.tags}</p>
+          <div dangerouslySetInnerHTML={{ __html: node.html}} />
+        </div>
+      ))}
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   );
---
+++ version_gatsby_qiita/src/posts/post-1.md
@@ -0,0 +1,8 @@
+---
+path: '/blog/my-first-post'
+date: '2020-05-10'
+title: 'First Blog Post'
+tags: ['gatsby', 'qiita']
+---
+
+This is first post content.
