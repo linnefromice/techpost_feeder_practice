08b234689e4f6cf88802c1e94fd2614bd108032c
linnefromice <linnefromice@gmail.com>
2020-05-11 16:36:28 +0900

    ver gatsby qiita - use qiita api & display response (article)

--- version_gatsby_qiita/src/pages/page-2.tsx
+++ version_gatsby_qiita/src/pages/page-2.tsx
@@ -1,17 +1,44 @@
 // Gatsby supports TypeScript natively!
-import React from "react"
+import React, { FunctionComponent, useState, useEffect } from "react"
 import { PageProps, Link } from "gatsby"
+import axios from "axios";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = (props: PageProps) => (
-  <Layout>
+const SecondPage:FunctionComponent = (props: PageProps) => {
+  const [datas, setDatas] = useState([]);
+  useEffect(() => {
+    axios.get(`http://qiita.com/api/v2/items`)
+      .then(res => {
+        setDatas(res.data);
+      })
+      .catch(res => {
+        console.log(res);
+      });
+  }, [])
+
+  return <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2 ({props.path})</p>
     <Link to="/">Go back to the homepage</Link>
+    {datas.map((data) => (
+        <div key={data.id}>
+          <h3>{data.title}</h3>
+          <p>{data.user.name}</p>
+          <p>{data.created_at}</p>
+          <a href={data.url}>LINK</a>
+        </div>
+     ))}
   </Layout>
-)
+}
+
+/*
+const userId = 'ko2ic';
+axios.get(`http://qiita.com/api/v2/users/${userId}/items`)
+  .then(res => console.log(res))
+  .catch(res => console.log(res));
+*/
 
 export default SecondPage
